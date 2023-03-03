1b1dad58554e545d41c3584e0e2f5ad3d64850d1
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2023-03-03 11:54:40 +0100

    chore(ci): refactoring of build pipeline
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- ci-scripts/generateHtmlReport.py
+++ ci-scripts/generateHtmlReport.py
@@ -474,9 +474,18 @@ class HtmlReport():
 				json_build_start = False
 				json_build_status = False
 				base_image = False
+				build_stage_id = 'NotAcorrectBuildStageId'
 				with open(cwd + '/archives/' + logFileName, 'r') as logfile:
 					for line in logfile:
+						# old method
 						result = re.search('FROM oai-nssf-base:latest', line)
+						if result is not None:
+							base_image = True
+						# new method --> buildx may cache this stage
+						result = re.search('^#([0-9]+).* RUN ./build_nssf --install-deps', line)
+						if result is not None:
+							build_stage_id = result.group(1)
+						result = re.search(f'^#{build_stage_id} CACHED', line)
 						if result is not None:
 							base_image = True
 						result = re.search(section_start_pattern, line)
@@ -691,8 +700,8 @@ class HtmlReport():
 		for variant in variants:
 			logFileName = 'nssf_' + variant + '_image_build.log'
 			if os.path.isfile(cwd + '/archives/' + logFileName):
-				section_start_pattern = '[aA][sS] oai-nssf$'
-				section_end_pattern = 'WORKDIR /openair-nssf/etc'
+				section_start_pattern = 'COPY --from=oai-nssf-builder */openair-nssf/build/nssf/build/oai_nssf'
+				section_end_pattern = 'COPY --from=oai-nssf-builder */openair-nssf/etc/nssf.conf '
 				section_status = False
 				status = False
 				with open(cwd + '/archives/' + logFileName, 'r') as logfile:
@@ -739,7 +748,7 @@ class HtmlReport():
 			if os.path.isfile(cwd + '/archives/' + logFileName):
 				section_start_pattern = 'WORKDIR /openair-nssf/etc'
 				if variant == 'docker':
-					section_end_pattern = 'Successfully tagged oai-nssf'
+					section_end_pattern = 'naming to docker.io/library/oai-nssf:'
 				else:
 					section_end_pattern = 'COMMIT oai-nssf:'
 				section_status = False
@@ -790,34 +799,32 @@ class HtmlReport():
 			if os.path.isfile(cwd + '/archives/' + logFileName):
 				if nfType == 'NSSF':
 					if variant == 'docker':
-						section_start_pattern = 'Successfully tagged oai-nssf'
+						section_start_pattern = 'naming to docker.io/library/oai-nssf:'
 						section_end_pattern = 'OAI-NSSF DOCKER IMAGE BUILD'
 					else:
 						section_start_pattern = 'COMMIT oai-nssf:'
 						section_end_pattern = 'OAI-NSSF PODMAN RHEL8 IMAGE BUILD'
 				section_status = False
 				status = False
+				imageTag = 'notAcorrectTagForTheMoment'
 				with open(cwd + '/archives/' + logFileName, 'r') as logfile:
 					for line in logfile:
-						result = re.search(section_start_pattern, line)
+						result = re.search(f'{section_start_pattern}([0-9a-zA-Z\-\_\.]+)', line)
 						if result is not None:
 							section_status = True
+							imageTag = result.group(1)
 						result = re.search(section_end_pattern, line)
 						if result is not None:
 							section_status = False
 						if section_status:
 							if nfType == 'NSSF':
-								if self.git_pull_request:
-									result = re.search('oai-nssf *ci-tmp', line)
-								else:
-									result = re.search('oai-nssf *develop', line)
+								result = re.search(f'oai-nssf *{imageTag}', line)
 							if result is not None and not status:
-								if variant == 'docker':
-									result = re.search('ago *([0-9A-Z]+)', line)
-								else:
-									result = re.search('ago *([0-9]+ [A-Z]+)', line)
+								result = re.search('ago  *([0-9A-Z ]+)', line)
 								if result is not None:
 									size = result.group(1)
+									if variant == 'docker':
+										size = re.sub('MB', ' MB', size)
 									status = True
 					logfile.close()
 				if status:

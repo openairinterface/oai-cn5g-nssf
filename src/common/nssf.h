e18380fdd0e1c8b845bf9609f861d0ebd9215321
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2024-07-03 11:17:03 +0200

    Sync with common src/build/ci and remove duplicated files

--- src/common/nssf.h
+++ src/common/nssf.h
@@ -25,8 +25,6 @@
 #include "3gpp_29.571.h"
 #include <nlohmann/json.hpp>
 
-const uint8_t SST_MAX_STANDARDIZED_VALUE = 127;
-
 // SMF + AMF + 3GPP TS 29.571 (Common data)
 enum class http_response_codes_e {
   HTTP_RESPONSE_CODE_OK                     = 200,
